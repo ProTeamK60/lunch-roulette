e011a697101cd3999cdafb1ea81eef2648c87da5
anton.olovsson <anton.olovsson@knowit.se>
2020-12-02 13:03:30 +0100

    Added splash activity

--- app/src/main/java/com/example/lunchroulette/activities/MainActivity.kt
+++ app/src/main/java/com/example/lunchroulette/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.lunchroulette
+package com.example.lunchroulette.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lunchroulette.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/lunchroulette/activities/Splash.kt
@@ -0,0 +1,15 @@
+package com.example.lunchroulette.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.lunchroulette.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({startActivity(Intent(this,MainActivity::class.java))}, 3000)
+    }
+}
