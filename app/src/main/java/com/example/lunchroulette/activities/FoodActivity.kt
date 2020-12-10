07ffb3275a1a9de6ac2679b7078481c2a4963c80
AideenStrand <aideen.strandsson@gmail.com>
2020-12-10 13:03:57 +0100

    added food activity

---
+++ app/src/main/java/com/example/lunchroulette/activities/FoodActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lunchroulette.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lunchroulette.R
+
+class FoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+
+
+    }
+}
--- app/src/main/java/com/example/lunchroulette/activities/Splash.kt
+++ app/src/main/java/com/example/lunchroulette/activities/Splash.kt
@@ -10,6 +10,6 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed({startActivity(Intent(this,MainActivity::class.java))}, 3000)
+        Handler().postDelayed({startActivity(Intent(this,FoodActivity::class.java))}, 3000)
     }
 }
